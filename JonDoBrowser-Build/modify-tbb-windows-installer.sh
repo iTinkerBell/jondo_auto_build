3f67eb1fd34043dd95ec5166cfffa1116c93847b
iTinkerBell <tinkerbel1988@outlook.com>
2017-05-10 09:45:40 +0800

    windows installer update for branding

--- JonDoBrowser-Build/modify-tbb-windows-installer.sh
+++ JonDoBrowser-Build/modify-tbb-windows-installer.sh
@@ -6,5 +6,4 @@ git checkout master
 git grep -l 'TorBrowser' | xargs sed -i 's/TorBrowser/JonDoBrowser/g'
 git grep -l 'Tor Browser' | xargs sed -i 's/Tor Browser/JonDoBrowser/g'
 #commit to local git repo
-echo $tbb_windows_installer_version
-source "$project_dir/local-commit.sh" $tbb_windows_installer_version
+source "$project_dir/local-commit.sh" "v$tbb_windows_installer_version"
