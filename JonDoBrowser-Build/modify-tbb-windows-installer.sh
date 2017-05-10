491f8274973e5eb0b7db6846865fa91d715b8d9e
iTinkerBell <tinkerbel1988@outlook.com>
2017-05-10 09:26:19 +0800

    windows installer update

--- JonDoBrowser-Build/modify-tbb-windows-installer.sh
+++ JonDoBrowser-Build/modify-tbb-windows-installer.sh
@@ -2,7 +2,7 @@
 #modify tbb-windows-installer addon local repo
 cd $project_dir
 cd ../local-repo/tbb-windows-installer-local
-git checkout $tbb_windows_installer_version
+git checkout master
 git grep -l 'TorBrowser' | xargs sed -i 's/TorBrowser/JonDoBrowser/g'
 git grep -l 'Tor Browser' | xargs sed -i 's/Tor Browser/JonDoBrowser/g'
 #commit to local git repo
